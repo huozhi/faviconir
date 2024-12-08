cce1923b8fe26434024285d221fc1ec4cd59d2bd
Jiachi Liu <inbox@huozhi.im>
2024-12-08 01:30:20 +0100

    fix metadata override

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
 import Faviconir from '@/components/faviconir'
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Faviconir - Generate Custom Favicons',
-  description: 'Create unique favicons with customizable shapes, colors, and backgrounds.',
-}
 
 export default function Home() {
   return (
